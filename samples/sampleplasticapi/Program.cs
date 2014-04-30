728a2228ebf989d3908fe0337491679bd8e15ef4
mgonzalez <mgonzalez@.(none)>
2014-04-30 11:37:14 +0200

    Create the sample API solution

---
+++ samples/sampleplasticapi/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace sampleplasticapi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
